e727a580ae079e1ca3114da35a0fca901361c64c
Shahenda Elshayal <shahendaelshayal@Shahendas-MacBook-Pro.local>
2025-07-01 19:01:00 +0300

    test fonts

---
+++ main.js
@@ -0,0 +1,52 @@
+// function func(x, y) {
+//     if (x > 3) {
+//         var a = 10;
+//     }
+//     else {
+//         var b = 5;
+//     }
+//     console.log(a, b);
+//     c = 20;
+// }
+
+// console.log(c);
+// func(1, 2);
+
+
+// var book = {
+//     id :2,
+//     "main title": "JavaScript",
+// }
+
+// console.log(book."main title");
+
+
+// let names = ["John", "JANE", "Jack", "Jill"]
+// console.log(names.includes("jane".toUpperCase()));
+
+
+var button = document.getElementById("onlyNumber");
+ 
+button.addEventListener('input', function(e) {
+    // let input = e.target.value;
+    // input = input.replace(/[^0-9]/g, '');
+    // e.target.value = input;
+
+    console.log(window.location.pathname);
+});
+ 
+// var numInput = document.getElementById("onlyNumber");
+
+// numInput.addEventListener("input",function(e){
+//     let input = e.target.value;
+//     input  = input.replace(/[^0-9]/g,"");
+//     e.target.value = input;
+// })
+
+// for(var i = 0;i<4;i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     },1000);
+// }
+
+// console.log(window.location.pathname);
